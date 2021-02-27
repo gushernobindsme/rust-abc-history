5b0607c32e07e8423279b399fc8b38bacead7203
Takumi ERA <takumi_era@caddi.jp>
2021-02-28 01:59:26 +0900

    fix

--- abc193d/src/main.rs
+++ abc193d/src/main.rs
@@ -2,73 +2,67 @@
 
 use itertools::Itertools;
 use proconio::input;
-use std::collections::BTreeMap;
 
 fn main() {
     input! {
         k: u32,  // 1〜9のカードが何セットあるか
-        s: String, // 高橋くん
-        t: String, // 青木くん
+        s: String, // 高橋くんの手札
+        t: String, // 青木くんの手札
+    }
+    let mut deck = [k; 9];
+    let s = s.bytes().take(4).map(|x| (x - b'0') as usize).collect_vec();
+    let t = t.bytes().take(4).map(|x| (x - b'0') as usize).collect_vec();
+
+    // 山札から使用済みのカードを取り除く
+    for i in s.clone() {
+        deck[i - 1] -= 1;
+    }
+    for i in t.clone() {
+        deck[i - 1] -= 1;
     }
 
-    // 山札にカードをセット
-    let mut deck = BTreeMap::new(); // (カードの種類, 残り枚数)
+    let mut answer = 0_f64;
     for i in 1..=9 {
-        deck.insert(i, k);
-    }
-
-    // 高橋くんにカードを配る
-    let mut t_score = 0;
-    let t_cards = s
-        .chars()
-        .filter(|v| v.to_string() != "#")
-        .collect::<Vec<_>>();
-    for card in t_cards {
-        let suit = card.to_string().parse::<i32>().unwrap();
-        if let Some(quantity) = deck.get(&suit) {
-            deck.insert(suit, quantity - 1);
-            t_score += suit;
-        }
-    }
-    // 青木くんにカードを配る
-    let mut a_score = 0;
-    let a_cards = t
-        .chars()
-        .filter(|v| v.to_string() != "#")
-        .collect::<Vec<_>>();
-    for card in a_cards {
-        let suit = card.to_string().parse::<i32>().unwrap();
-        if let Some(quantity) = deck.get(&suit) {
-            deck.insert(suit, quantity - 1);
-            a_score += suit;
-        }
-    }
-
-    // 山札から残りのカードを取り出す
-    let mut remain_cards = Vec::new();
-    for (suit, quantity) in deck {
-        if quantity == 0 {
+        if deck[i - 1] == 0 {
             continue;
         }
-        for _ in 1..=quantity {
-            remain_cards.push(suit);
+        for j in 1..=9 {
+            if i == j || deck[j - 1] == 0 {
+                continue;
+            }
+            if score(s.iter().copied().chain(vec![i]).collect())
+                > score(t.iter().copied().chain(vec![j]).collect())
+            {
+                answer += deck[i - 1] as f64 * deck[j - 1] as f64
+            }
         }
     }
 
-    // 残りカードの順列を取得
-    let combinations = remain_cards
-        .iter()
-        .permutations(2)
-        .map(|v| (v[0], v[1]))
-        .collect::<Vec<_>>();
-
-    // 高橋くんが勝利するケースを求める
-    let mut count = 0;
-    for combination in combinations.clone() {
-        let (t_last_card, a_last_card) = combination;
-        if (t_score + t_last_card) > (a_score + a_last_card) {
-            count += 1;
+    for i in 1..=9 {
+        if deck[i - 1] < 2 {
+            continue;
+        }
+        if score(s.iter().copied().chain(vec![i]).collect())
+            > score(t.iter().copied().chain(vec![i]).collect())
+        {
+            answer += deck[i - 1] as f64 * (deck[i - 1] - 1) as f64
         }
     }
-    println!("{}", (count as f64 / combinations.len() as f64));
+
+    let total = 9 * k - 8;
+    println!("{}", (answer as f64 / total as f64 / (total - 1) as f64));
+}
+
+/// 手札の配列を元に得点を計算する
+fn score(cards: Vec<usize>) -> i32 {
+    let mut count = [0; 9];
+    for card in cards {
+        count[card - 1] += 1;
+    }
+
+    let mut score = 0;
+    for i in 1..=9 {
+        score += i as i32 * 10_i32.pow(count[i - 1]);
+    }
+    score
 }
